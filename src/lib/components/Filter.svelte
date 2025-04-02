d33aead43aa7ff5e8b79ce71289f794dfd9ad36f
Asahel Zavaleta <abraham_zm22@hotmail.com>
2025-04-02 05:04:07 -0500

    add @tanstack/svelte-query for data fetching and implement query handling in Filter component

--- src/lib/components/Filter.svelte
+++ src/lib/components/Filter.svelte
@@ -2,6 +2,7 @@
 	import db from '$lib/db/db';
 	import { pokemonFeedItems } from '$lib/store/persistStore';
 	import type { Filter, Item } from '$lib/types/types';
+	import { createQuery } from '@tanstack/svelte-query';
 	import { liveQuery } from 'dexie';
 	import { onMount } from 'svelte';
 
@@ -14,6 +15,19 @@
 	const access = (str: string) => {
 		return $pokemonFeedItems[str as keyof typeof $pokemonFeedItems];
 	};
+
+	let queryResult = createQuery<Item, Error>({
+		queryKey: ['item', filter.id],
+		queryFn: async () => await db.feedItem.where('query').equals(query).toArray()
+		// queryFn: async () =>
+		// 	liveQuery(async () => {
+		// 		// await new Promise((resolve) => setTimeout(resolve, 2000));
+
+		// 		return db.feedItem.where('query').equals(query).toArray();
+		// 	})
+		// queryFn: async () =>
+		// 	await fetch('https://api.github.com/repos/SvelteStack/svelte-query').then((r) => r.json())
+	});
 
 	let localItems = liveQuery(async () => {
 		// await new Promise((resolve) => setTimeout(resolve, 2000));
@@ -30,9 +44,12 @@
 	// })
 	function onClickItem({ item }: { item: Item }) {
 		db.feedItem.put({ ...item, isCopied: !item.isCopied });
+
+		$queryResult.refetch();
 	}
 </script>
 
+<!-- {JSON.stringify($queryResult)} -->
 <div class="flex flex-col gap-2 bg-neutral-800">
 	<div class="bg-amber-950 hover:bg-amber-900">
 		<span>{name}</span>
@@ -40,7 +57,7 @@
 		<span>{($localItems ?? []).length}</span>
 	</div>
 	<div class="flex flex-col gap-4">
-		{#await $localItems}
+		<!-- {#await $localItems}
 			Loading....xc
 		{:then items}
 			{#each items as item}
@@ -56,7 +73,30 @@
 					<div>{item.isCopied}</div>
 				</button>
 			{/each}
-		{/await}
+		{/await} -->
+
+		{#if $queryResult.isLoading}
+			Loading...
+		{/if}
+		{#if $queryResult.error}
+			An error has occurred:
+			{$queryResult.error.message}
+		{/if}
+		{#if $queryResult.isSuccess}
+			{#each $queryResult.data as item}
+				<button
+					class="flex cursor-pointer flex-col bg-violet-950 hover:bg-violet-900"
+					class:is-copied={item.isCopied}
+					onclick={() => onClickItem({ item })}
+				>
+					<div>{item.name}</div>
+					<div>{item.ivs}</div>
+					<div>{item.localTimeZone}</div>
+					<div>{item.locationName}</div>
+					<div>{item.isCopied}</div>
+				</button>
+			{/each}
+		{/if}
 
 		<!-- {#each items as item}
 			<div class="flex flex-col bg-violet-950 hover:bg-violet-900">
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,20 @@
 <script lang="ts">
 	import '../app.css';
+	import { browser } from '$app/environment';
+	import { QueryClientProvider, QueryClient } from '@tanstack/svelte-query';
 
 	let { children } = $props();
+	const queryClient = new QueryClient({
+		defaultOptions: {
+			queries: {
+				enabled: browser
+			}
+		}
+	});
 </script>
 
-<div class="bg-neutral-500 h-screen text-white overflow-hidden">
-{@render children()}
-</div>
+<QueryClientProvider client={queryClient}>
+	<div class="h-screen overflow-hidden bg-neutral-500 text-white">
+		{@render children()}
+	</div>
+</QueryClientProvider>
